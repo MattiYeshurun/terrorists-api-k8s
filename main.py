c89a962e74df4ace029640750fba7e69c820db23
Matti <m0527176210@gmail.com>
2026-01-15 10:59:29 +0200

    build my app

---
+++ main.py
@@ -0,0 +1,29 @@
+import csv
+import io
+from fastapi import FastAPI, File, HTTPException, UploadFile
+import multipart
+import uvicorn
+
+app = FastAPI()
+
+@app.post("/top-threats")
+def upload_csv(file: UploadFile = File(...)):
+    if file.content_type != "text/csv":
+        raise HTTPException(status_code=400, detail="File must be a CSV")
+
+    content = file.file.read()
+
+    decoded = content.decode("utf-8")
+
+    reader = csv.reader(io.StringIO(decoded))
+    rows = [row for row in reader]
+
+    return {
+        "message": "CSV load",
+        "rows_count": len(rows),
+        "data": rows,
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8080)
